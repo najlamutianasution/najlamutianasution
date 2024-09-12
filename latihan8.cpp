4c75f5a285a1e88fa69f448926d2739a70f0d7b7
najlamutianasution <najlanasution8@gmail.com>
2024-09-12 13:22:40 +0700

    Add files via upload

---
+++ latihan8.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<iomanip>
+    
+using namespace std;
+
+int main()
+{
+
+     int bil;
+     
+     cout<<"KETIKA ANGKA DARI [0..9] =";
+     cin>>bil;
+     switch(bil){
+ case 0:
+      cout<<"ANDA MENGETIKKAN ANGKA 0";
+      break;
+  case 1:
+      cout<<"ANDA MENGETIKKAN ANGKA 1";
+      break;
+   case 2:
+          cout<<"ANDA MENGETIKKAN ANGKA 2";
+          break;
+          
+            case 3:
+            cout<<"ANDA MENGETIKKAN ANGKA 3";
+            break;
+            
+                case 4:
+                cout<<"ANDA MENGETIKKAN ANGKA 4";
+                break;
+                
+                  case 5:
+                  cout<<"ANDA MENGETIKKAN ANGKA 5";
+                  break;
+                  
+                   case 6:
+                   cout<<"ANDA MENGETIKKAN ANGKA 6";
+                   break;
+                   
+                   case 7:
+                   cout<<"ANDA MENGETIKKAN ANGKA 7";
+                   break;
+                   
+                   case 8:
+                   cout<<"ANDA MENGETIKKAN ANGKA 8";
+                   break;
+                   
+                   case 9:
+                   cout<<"ANDA MENGETIKKAN ANGKA 9";
+                   break;
+                   
+                   default:
+                        cout<<"NILAI TIDAK VALID! " ;
+      }
+return 0;
+}           
+               
