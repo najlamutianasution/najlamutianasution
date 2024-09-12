6e260ef4fb62906c7c870506a92866f017c5a65a
najlamutianasution <najlanasution8@gmail.com>
2024-09-12 13:20:09 +0700

    Add files via upload

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+//dibuat oleh NAJLAMUTIANASUTION
+//tanggal dibuat 2 SEPTEMBER 
+#include<iostream>
+using namespace std; //perintah untuk membuat variabel std
+int main()
+{
+
+cout<<"STMIK PELITA NUSANTARA"<<endl;
+cout<<"Algoritma pemrograman" <<endl;
+cout<< 5 <<endl;
+cout<<'A'<<endl;
+return 0;
+}
+
+
+
+
+
+
+    
