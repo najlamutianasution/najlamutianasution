4285fcb094ae5ebbb35fe5083da03192b4e2ba30
najlamutianasution <najlanasution8@gmail.com>
2024-09-12 13:21:05 +0700

    Add files via upload

---
+++ latihan3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int luas,panjang,lebar;
+cout<<"masukkan nilai panjang =";
+cin>>panjang;
+cout<<"masukkan nilai lebar=";
+cin>>lebar;
+luas=panjang*lebar;
+cout<<"luas persegipanjang="<<luas<<endl;
+return 0;
+}
+
