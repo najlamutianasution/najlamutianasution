6edd965641fe8931893bbcde6af4dfaec0379ca8
najlamutianasution <najlanasution8@gmail.com>
2024-09-12 13:21:24 +0700

    Add files via upload

---
+++ latihan4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float Luas,Alas,Tinggi;
+cout<<"input Alas=";
+cin>>Alas;
+cout<<"input Tinggi=";
+cin>>Tinggi;
+Luas=0.5*Alas*Tinggi;
+cout<<"Luas Segitiga="<<Luas<<endl;
+return 0;
+}
