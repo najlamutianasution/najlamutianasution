6079d8bf76a5f240227dd186809cb98e1588d498
najlamutianasution <najlanasution8@gmail.com>
2024-11-28 23:05:08 +0700

    Add files via upload

---
+++ latihan36.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	  ofstream file("data.txt");
+	  
+	  if (file.is_open()) {
+	  	file << "STMIK Penusa\n";
+	  	file << "Belajar Algoritma Pemrograman.";
+	  	file.close();
+} else {
+	   cout << "File tidak bisa dibuka";
+}
+
+	   return 0;
+}	   	  		   	  	
